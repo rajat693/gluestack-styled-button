37355fc4fd53667fb9b8e98b892edabd806cfa68
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-03-20 00:18:58 +0530

    feat: updated layout

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { StyledProvider } from "@gluestack-style/react";
 import { config } from "./gluestack-style.config";
-import { View } from "react-native";
-import Button from "./styled-components";
+import { Text, View } from "react-native";
+import { StyledButton } from "./styled-components/Button";
+import { StyledText } from "./styled-components/Text";
+import { createButton } from "./components";
+
+const Button = createButton(StyledButton, StyledText);
 
 const App = () => {
+  const ref = useRef(null);
   return (
     <StyledProvider config={config}>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button variant="ghost" action="secondary">
-          <Button.Text>hello world</Button.Text>
+        <Button variant="ghost" action="secondary" ref={ref}>
+          {/* <Text>Hello</Text> */}
+          <Button.Text>Hello</Button.Text>
         </Button>
       </View>
     </StyledProvider>
